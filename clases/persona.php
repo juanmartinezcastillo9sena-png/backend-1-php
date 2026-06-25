d46eaef559aa5294f888ffad48c30d4b756c34a9
Juan Martinez <juanmartinezcastillo9sena@gmail.com>
2026-06-25 17:12:42 -0500

    Acciones POO

--- clases/persona.php
+++ clases/persona.php
@@ -3,12 +3,13 @@ class Persona
 {
     public function saludar()
     {
-        echo "Este es el primer ejercicio con POO";
+        echo "Este es el primer ejercicio con POO<br>";
+    }
+
+    public function despedirse()
+    {
+        echo "Se acabo la clase<br>";
     }
 }
 
-class Calculadora
-{
-
-}
 ?>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "../clases/persona.php";
+
+$juan = new Persona();
+
+$juan->saludar();
+
+$juan->despedirse();
+?>
